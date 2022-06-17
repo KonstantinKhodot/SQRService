1d919e11e9e6bc5bc360299920528ec8315b5a88
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-06-17 12:53:49 +0300

    Test4 commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,16 +4,14 @@ public class SQRService {
 
     public int sqrNum(int lowerLimit, int upperLimit) {
         int counter = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 10; i <= upperLimit; i++) {
             if (i * i >= lowerLimit) {
-                System.out.println("Нижняя граница" + i);
                 if (i * i <= upperLimit) {
-                    System.out.println("Верхняя граница" + i);
-
+                    counter = counter++;
                 }
             }
-            counter = counter++;
         }
-        return counter++;
+        System.out.println(counter);
+        return counter;
     }
 }
